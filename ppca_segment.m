401fbbbb577f37a3d53034c8dd852f84d40c9106
jcwu <seedmanee@gmail.com>
2012-03-08 03:44:58 -0800

    ppca

--- ppca_segment.m
+++ ppca_segment.m
@@ -1,16 +1,17 @@
-function [cut_frame] = pca_segment(X_total)
-  tau = 0.9;
+function [cut_frame] = ppca_segment(X_total)
+  tau = 0.95;
 
-  frame_num = size(X_total, 1);
+  [frame_num n] = size(X_total);
   cut_frame = frame_num;
 
-  k = 240;  % window size
-  l =  60;
+  Delta = 10;
+  T = 150;
+  K = T;  
+  R = 15; % threshold
 
-  i = k;
-  i0 = k + l;
+  H = zeros(frame_num, 1);
 
-  if (frame_num < i0)
+  if (frame_num < T)
     return;
   end
 
@@ -20,46 +21,37 @@ function [cut_frame] = pca_segment(X_total)
   d_mean = zeros(frame_num, 1);
   d_std  = zeros(frame_num, 1);
 
-  % fit r with first k frames
-  X_mean = mean(X_total(1:k,:));
-  Xfit = X_total(1:k, :) - ones(k,1) * X_mean;
+  % fit r with first K frames
+ % X_mean = mean(X_total(1:K,:));
+  X_mean = mean(X_total);
+  Xfit = X_total(1:K, :) - ones(K,1) * X_mean;
   [U S V] = svd(Xfit, 0);
 
-  r = 4;
+  r = 1;
   while Er(diag(S),r) < tau
     r = r + 1;
   end
-%  disp(r);
+  disp(r);
+  
+  % ppca part
+  diagS = diag(S);
 
-  % svd for every frame
-  for i = k: frame_num
-    X = X_total(1:i,:);
+  avg_square_discard = mean(diagS(r+1:n) .* diagS(r+1:n));
 
-    [m, n] = size(X);
-    D = X - ones(m,1) * mean(X);
-    [U S V] = svd(D,0);
+  W = V(:,1:r) * sqrt( S(1:r,1:r)^2 - avg_square_discard*eye(r));
+  C = (W*W' + avg_square_discard*eye(n))/(frame_num-1);
+  invC = inv(C);
 
-    proj_error(i) = project_error(diag(S), r);
-    delta_error(i) = proj_error(i) - proj_error(i-l);
-
-    d_mean(i) = mean(delta_error(k:i));
-    d_std(i) = std(delta_error(k:i));
-
-    if ( i > i0 && (delta_error(i) > d_mean(i) + 3 * d_std(i)) )
-      cut_frame = i;
+  for K = T:Delta:frame_num
+    if (K+T > frame_num)
       break;
     end
-  end % end of i
+    X_T = X_total(K+1:K+T,:);
+    X_zero_mean = X_T - ones(T,1)*X_mean;
+    H(K) = trace( X_zero_mean*invC*X_zero_mean' )/T;
+  end
 
-%  index = k:i;
-%  figure(1);
-%  hold on;
-%  plot(index, delta_error(index),'-b');
-%  plot(index, d_mean(index), ':r');
-%  legend('derivative error', 'mean');
-%  plot(index, d_mean(index)+d_std(index), ':m');
-%  plot(index, d_mean(index)-d_std(index), ':m');
-%  plot(index, d_mean(index)+3*d_std(index), ':g');
-%  plot(index, d_mean(index)-3*d_std(index), ':g');
-%  axis([0 i 0 max(d_mean + 3*d_std)]);
+  index = T:Delta:frame_num;
+  plot(index, H(index));
+
 end
